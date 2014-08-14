eda0678ea6ada5cc0f4714cbc4b22bf116925743
Leonard Truong <leonardtruong@gmail.com>
2014-08-14 12:34:34 -0700

    Bump for version release

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='stencil_code',
     description='A specializer built on the ASP SEJITS framework',
     url='https://github.com/ucb-sejits/stencil_code/',
-    version='0.95a2',
+    version='0.95a3',
 
     packages=[
         'stencil_code',
