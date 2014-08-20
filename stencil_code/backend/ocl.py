211faf8798ba6c199e9a2f980f24329fddde9865
Leonard Truong <leonardtruong@gmail.com>
2014-08-20 13:34:45 -0700

    functional clamping with OCL stencil

--- stencil_code/backend/ocl.py
+++ stencil_code/backend/ocl.py
@@ -1,5 +1,6 @@
 from ctree.c.nodes import Lt, Constant, And, SymbolRef, Assign, Add, Mul, Div, Mod, For, \
-    AddAssign, ArrayRef, FunctionCall, If, ArrayDef, Ref, FunctionDecl
+    AddAssign, ArrayRef, FunctionCall, If, ArrayDef, Ref, FunctionDecl, GtE, \
+    Sub, Cast
 from ctree.ocl.macros import get_global_id, get_local_id, get_local_size, clSetKernelArg, \
     NULL, barrier, CLK_LOCAL_MEM_FENCE
 from ctree.cpp.nodes import CppDefine
@@ -385,37 +386,26 @@ class StencilOclTransformer(StencilBackend):
 
     def gen_global_index(self):
         dim = len(self.output_grid.shape)
-        index = Add(get_global_id(dim - 1), Constant(self.ghost_depth))
+        index = get_global_id(dim - 1)
         for d in reversed(range(dim - 1)):
             index = Add(
                 Mul(
                     index,
                     Constant(self.output_grid.shape[d])
                 ),
-                Add(get_global_id(d), Constant(self.ghost_depth))
+                get_global_id(d)
             )
         return index
 
     def load_shared_memory_block(self, target, padding):
         dim = len(self.output_grid.shape)
         body = []
-        thread_id = get_local_id(0)
-        num_threads = get_local_size(0)
-        block_size = Add(get_local_size(0), padding)
-        for d in range(1, dim):
-            thread_id = Add(
-                Mul(get_local_id(d), get_local_size(d - 1)),
-                thread_id
-            )
-            num_threads = Mul(get_local_size(d), num_threads)
-            block_size = Mul(
-                Add(get_local_size(d), padding),
-                block_size
-            )
+        thread_id, num_threads, block_size = gen_decls(dim, padding)
 
-        body.append(Assign(SymbolRef("thread_id", ct.c_int()), thread_id))
-        body.append(Assign(SymbolRef("block_size", ct.c_int()), block_size))
-        body.append(Assign(SymbolRef("num_threads", ct.c_int()), num_threads))
+        body.extend([Assign(SymbolRef("thread_id", ct.c_int()), thread_id),
+                     Assign(SymbolRef("block_size", ct.c_int()), block_size),
+                     Assign(SymbolRef("num_threads", ct.c_int()), num_threads)
+                     ])
         base = None
         for i in reversed(range(0, dim - 1)):
             if base is not None:
@@ -479,25 +469,27 @@ class StencilOclTransformer(StencilBackend):
                 Assign(SymbolRef('tid', ct.c_int()), SymbolRef('thread_id')),
                 Lt(SymbolRef('tid'), SymbolRef('block_size')),
                 AddAssign(SymbolRef('tid'), SymbolRef('num_threads')),
-                local_indices + [
-                    Assign(
-                        ArrayRef(
-                            target,
-                            SymbolRef('tid')
-                        ),
-                        ArrayRef(
-                            SymbolRef(self.input_names[0]),
-                            self.global_array_macro(
-                                [Add(
-                                    SymbolRef("local_id%d" % d),
-                                    Mul(FunctionCall(SymbolRef('get_group_id'),
-                                                     [Constant(d)]),
-                                        get_local_size(d))
-                                ) for d in range(0, dim)]
-                            )
+                local_indices + [Assign(
+                    ArrayRef(
+                        target,
+                        SymbolRef('tid')
+                    ),
+                    ArrayRef(
+                        SymbolRef(self.input_names[0]),
+                        self.global_array_macro(
+                            [FunctionCall(SymbolRef('clamp'),
+                                          [Cast(ct.c_int(), Sub(Add(
+                                SymbolRef("local_id%d" % d),
+                                Mul(FunctionCall(SymbolRef('get_group_id'),
+                                                 [Constant(d)]),
+                                    get_local_size(d))
+                            ), Constant(self.kernel.ghost_depth))),
+                                Constant(0), Constant(self.arg_cfg[0].shape[d])
+                            ]
+                            ) for d in range(0, dim)]
                         )
                     )
-                ]
+                )]
             )
         )
         return body
@@ -505,13 +497,21 @@ class StencilOclTransformer(StencilBackend):
     def visit_InteriorPointsLoop(self, node):
         dim = len(self.output_grid.shape)
         self.kernel_target = node.target
-        cond = Lt(get_global_id(0),
-                  Constant(self.arg_cfg[0].shape[0] - self.ghost_depth * 2))
+        cond = And(
+            Lt(get_global_id(0),
+               Constant(self.arg_cfg[0].shape[0] - self.ghost_depth)),
+            GtE(get_global_id(0),
+                Constant(self.ghost_depth))
+        )
         for d in range(1, len(self.arg_cfg[0].shape)):
             cond = And(
                 cond,
-                Lt(get_global_id(d),
-                   Constant(self.arg_cfg[0].shape[d] - self.ghost_depth * 2))
+                And(
+                    Lt(get_global_id(d),
+                       Constant(self.arg_cfg[0].shape[d] - self.ghost_depth)),
+                    GtE(get_global_id(d),
+                        Constant(self.ghost_depth))
+                )
             )
         body = []
 
@@ -574,3 +574,20 @@ class StencilOclTransformer(StencilBackend):
             return ArrayRef(SymbolRef(grid_name), self.visit(target))
         raise Exception(
             "Unsupported GridElement encountered: {0}".format(grid_name))
+
+
+def gen_decls(dim, padding):
+    thread_id = get_local_id(0)
+    num_threads = get_local_size(0)
+    block_size = Add(get_local_size(0), padding)
+    for d in range(1, dim):
+        thread_id = Add(
+            Mul(get_local_id(d), get_local_size(d - 1)),
+            thread_id
+        )
+        num_threads = Mul(get_local_size(d), num_threads)
+        block_size = Mul(
+            Add(get_local_size(d), padding),
+            block_size
+        )
+    return thread_id, num_threads, block_size
