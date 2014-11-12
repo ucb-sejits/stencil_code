7bc52a899e7a17a6fdb559f31ea9494a9dc8ccb9
chick <chick@qrhino.com>
2014-11-11 22:45:59 -0800

    jacobi works

--- examples/jacobi_stencil.py
+++ examples/jacobi_stencil.py
@@ -1,25 +1,26 @@
-from stencil_code.stencil_kernel import StencilKernel
+from stencil_code.stencil_kernel2 import Stencil
 import numpy
+import numpy.testing
 import logging
 
 logging.basicConfig(level=20)
 
 
-class Jacobi(StencilKernel):
-    neighbor_definition = [[(0, -1), (0, 1)], [(-1, 0), (1, 0)]]
+class Jacobi(Stencil):
+    neighborhoods = [[(0, -1), (0, 1)], [(-1, 0), (1, 0)]]
 
     def kernel(self, in_grid, out_grid):
         for x in self.interior_points(out_grid):
             for y in self.neighbors(x, 0):
-                out_grid[x] += .1 *in_grid[y]
+                out_grid[x] += .1 * in_grid[y]
             for y in self.neighbors(x, 1):
                 out_grid[x] += .3 * in_grid[y]
 
-in_img = numpy.random.rand(1024, 1024).astype(numpy.float32) * 100
+in_img = numpy.random.random(1024, 1024).astype(numpy.float32) * 100
 
 jacobi_stencil = Jacobi(backend='ocl')
 py = Jacobi(backend='python')
 
 out_img = jacobi_stencil(in_img)
 check = py(in_img)
-numpy.testing.assert_array_almost_equal(out_img[1:-1, 1:-1], check[1:-1, 1:-1])
+numpy.testing.assert_array_almost_equal(out_img[1:-1, 1:-1], check[1:-1, 1:-1], decimal=4)
