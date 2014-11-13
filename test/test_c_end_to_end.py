ea2d2b522f78f5d56a5e2da233e0ba7e9a2b7f13
chick <chick@qrhino.com>
2014-11-12 23:09:37 -0800

    fixed up some pep erros in c end to end test

--- test/test_c_end_to_end.py
+++ test/test_c_end_to_end.py
@@ -1,31 +1,24 @@
 __author__ = 'leonardtruong'
 import unittest
 
-# from .examples.two_d_heat import TwoDHeatFlow
-# from examples.laplacian import LaplacianKernel
-# from examples.laplacian_27pt import SpecializedLaplacian27
-# from examples.better_bilateral_filter import BetterBilateralFilter
+from .kernels import TwoDHeatFlow, LaplacianKernel, BetterBilateralFilter
 
-from .kernels import TwoDHeatFlow, LaplacianKernel, SpecializedLaplacian27, BetterBilateralFilter
+import numpy
+import numpy.testing
 
-import numpy as np
-
-stdev_d = 3
-stdev_s = 70
-radius = 1
 width = 128
 height = width
 
 
 class TestCEndToEnd(unittest.TestCase):
     def setUp(self):
-        self.in_grid = np.random.rand(width, width).astype(np.float32) * 1000
+        self.in_grid = numpy.random.random([width, width]).astype(numpy.float32) * 1000
 
     def _check(self, test_kernel):
         outgrid1 = test_kernel(backend='c')(self.in_grid)
         outgrid2 = test_kernel(backend='python')(self.in_grid)
         try:
-            np.testing.assert_array_almost_equal(outgrid1, outgrid2)
+            numpy.testing.assert_array_almost_equal(outgrid1, outgrid2)
         except Exception:
             self.fail("Output grids not equal")
 
@@ -35,15 +28,12 @@ class TestCEndToEnd(unittest.TestCase):
     def test_laplacian(self):
         self._check(LaplacianKernel)
 
-    def test_laplacian(self):
-        self._check(LaplacianKernel)
-
     def test_bilateral_filter(self):
-        in_grid = np.random.rand(width, height).astype(np.float32) * 255
+        in_grid = numpy.random.random([width, height]).astype(numpy.float32) * 255
         out_grid1 = BetterBilateralFilter(backend='c')(in_grid)
         out_grid2 = BetterBilateralFilter(backend='python')(in_grid)
 
         try:
-            np.testing.assert_array_almost_equal(out_grid1, out_grid2)
+            numpy.testing.assert_array_almost_equal(out_grid1, out_grid2)
         except:
             self.fail("Output grids not equal")
