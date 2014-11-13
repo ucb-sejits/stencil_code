3b99cd4d0d4e723079113ec35fd2ac022e2903b2
chick <chick@qrhino.com>
2014-11-13 12:26:32 -0800

    c_end_to_end fixed up

--- test/test_ocl_end_to_end.py
+++ test/test_ocl_end_to_end.py
@@ -46,8 +46,8 @@ class TestCEndToEnd(unittest.TestCase):
         # print("o1 {}".format(out_grid1))
         print("o1 {}".format(out_grid1[3, 3:8]))
         print("o2 {}".format(out_grid2[3, 3:8]))
-        # try:
-        numpy.testing.assert_array_almost_equal(
-            out_grid1[3:-3, 3:-3], out_grid2[3:-3, 3:-3], decimal=1)
-        # except:
-        #     self.fail("Output grids not equal")
+        try:
+            numpy.testing.assert_array_almost_equal(
+                out_grid1[3:-3, 3:-3], out_grid2[3:-3, 3:-3], decimal=1)
+        except Exception as exception:
+            self.fail("Output grids not equal: {}".format(exception))
