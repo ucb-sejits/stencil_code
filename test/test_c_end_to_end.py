e378118fa3c7e340fba7943ac47b80fcbf3f4b1d
chick <chick@qrhino.com>
2014-11-14 11:13:37 -0800

    cool way of generating a testable interior points slice

--- test/test_c_end_to_end.py
+++ test/test_c_end_to_end.py
@@ -1,10 +1,13 @@
 __author__ = 'leonardtruong'
 import unittest
 
-from kernels import TwoDHeatFlow, LaplacianKernel, SpecializedLaplacian27, BetterBilateralFilter
-
 import numpy
 import numpy.testing
+
+from stencil_code.library.better_bilateral_filter import BetterBilateralFilter
+from stencil_code.library.laplacian import LaplacianKernel
+from stencil_code.library.laplacian_27pt import SpecializedLaplacian27
+from stencil_code.library.two_d_heat import TwoDHeatFlow
 
 width = 128
 height = 64
@@ -14,22 +17,35 @@ class TestCEndToEnd(unittest.TestCase):
     def setUp(self):
         self.in_grid = numpy.random.random([width, width]).astype(numpy.float32) * 1000
 
-    def _check(self, test_kernel):
-        outgrid1 = test_kernel(backend='c')(self.in_grid)
-        outgrid2 = test_kernel(backend='python')(self.in_grid)
+    def _check(self, stencil_class_to_test, in_grid=None):
+        if in_grid is None:
+            in_grid = self.in_grid
+
+        hp_stencil = stencil_class_to_test(backend='c')
+        python_stencil = stencil_class_to_test(backend='python')
+        hp_out_grid = hp_stencil(in_grid)
+        python_out_grid = python_stencil(in_grid)
+
+        interior_points_slice = tuple([slice(x, -x) for x in hp_stencil.ghost_depth])
         try:
-            numpy.testing.assert_array_almost_equal(outgrid1, outgrid2)
+            numpy.testing.assert_array_almost_equal(
+                hp_out_grid[interior_points_slice],
+                python_out_grid[interior_points_slice]
+            )
         except AssertionError:
             self.fail("Output grids not equal")
 
     def test_2d_heat(self):
-        self._check(TwoDHeatFlow)
+        in_grid = numpy.random.random([16, 16, 16]).astype(numpy.float32) * 1000
+        self._check(TwoDHeatFlow, in_grid)
 
     def test_laplacian(self):
-        self._check(LaplacianKernel)
+        in_grid = numpy.random.random([32, 32, 32]).astype(numpy.float32) * 1000
+        self._check(LaplacianKernel, in_grid)
 
     def test_laplacian27(self):
         in_grid = numpy.random.random([32, 32, 32]).astype(numpy.float32) * 255
+
         out_grid1 = SpecializedLaplacian27(backend='c')(in_grid)
         out_grid2 = SpecializedLaplacian27(backend='python')(in_grid)
 
