51b666061264c0ee2a5fc0b553d21f9ebbbf0eea
chick <chick@qrhino.com>
2014-11-18 13:00:40 -0800

    some tests for python clamping

--- test/test_boundary_handling.py
+++ test/test_boundary_handling.py
@@ -1,4 +1,5 @@
 import unittest
+from nose.tools import assert_list_equal
 import numpy
 from stencil_code.neighborhood import Neighborhood
 
@@ -19,6 +20,25 @@ class TestBoundaryHandling(unittest.TestCase):
         clamper = Clamper(backend='python', boundary_handling='clamp')
         in_grid = numpy.ones([10, 10])
 
-        clamper.current_shape = in_grid.shape
-        p = (9, 9)
-        print("{}.neighbors {}".format(p, [x for x in clamper.neighbors(p, 0)]))
+        clamper.current_shape = in_grid.shape   # this is ordinarily set by interior_points_loop
+
+        assert_list_equal(
+            [x for x in clamper.neighbors((9, 9), 0)],
+            [(8, 9), (9, 8), (9, 9), (9, 9), (9, 9)],
+            "clamping around (9, 9) should keep all values between 0 and 9 inclusive"
+        )
+        assert_list_equal(
+            [x for x in clamper.neighbors((0, 0), 0)],
+            [(0, 0), (0, 0), (0, 0), (0, 1), (1, 0)],
+            "clamping around (0, 0) should keep all between 0 and 9 inclusive"
+        )
+        assert_list_equal(
+            [x for x in clamper.neighbors((0, 0), 0)],
+            [(0, 0), (0, 0), (0, 0), (0, 1), (1, 0)],
+            "clamping around (0, 9) should keep all between 0 and 9 inclusive"
+        )
+        assert_list_equal(
+            [x for x in clamper.neighbors((3, 3), 0)],
+            [(2, 3), (3, 2), (3, 3), (3, 4), (4, 3)],
+            "neighborhoods around interior points are not clamped"
+        )
