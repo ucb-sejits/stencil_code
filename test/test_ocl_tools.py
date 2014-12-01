58a76d061093e8576b951193db732f5990857aee
chick <chick@qrhino.com>
2014-12-01 15:15:33 -0800

    some pep-8 cleanup

--- test/test_ocl_tools.py
+++ test/test_ocl_tools.py
@@ -1,6 +1,3 @@
-from stencil_code.boundary_kernel import BoundaryCopyKernel
-from stencil_code.neighborhood import Neighborhood
-
 __author__ = 'chick'
 
 import unittest
@@ -9,10 +6,10 @@ from stencil_code.backend.ocl_tools import product, OclTools
 
 
 class MockDevice(object):
-    def __init__(self, max_work_group_size=512, max_local_group_sizes=[512, 512, 512],
+    def __init__(self, max_work_group_size=512, max_local_group_sizes=None,
                  max_compute_units=40):
         self.max_work_group_size = max_work_group_size
-        self.max_local_group_sizes = max_local_group_sizes
+        self.max_local_group_sizes = max_local_group_sizes if max_local_group_sizes is not None else [512, 512, 512]
         self.max_compute_units = max_compute_units
 
 
