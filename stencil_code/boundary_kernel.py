16df077bf1cc29751f4567d64e31efb3ac4c1207
chick <chick@qrhino.com>
2014-12-03 12:11:17 -0800

    preparing to get going again with boundary kernel generation for ocl

--- stencil_code/boundary_kernel.py
+++ stencil_code/boundary_kernel.py
@@ -1,7 +1,7 @@
 from operator import mul
-import random
 import numpy
 from stencil_code.stencil_exception import StencilException
+from stencil_code.backend.ocl_tools import product, OclTools
 
 __author__ = 'chick'
 
@@ -11,17 +11,13 @@ from ctree.c.nodes import Lt, Constant, And, SymbolRef, Assign, Add, Mul, \
 from hindemith.fusion.core import KernelCall
 
 
-def product(vector):
-    reduce(mul, vector, 1)
-
-
 class BoundaryCopyKernel(object):
     """
     container for numbers necessary to generate and to call a kernel
     that copies boundary points from input to output during stencil
     copy boundary handling option
     """
-    def __init__(self, halo, grid, dimension, max_work_group_size=128):
+    def __init__(self, halo, grid, dimension, device=None):
         """
 
         :param halo: the halo shape
@@ -33,129 +29,23 @@ class BoundaryCopyKernel(object):
         self.grid = grid
         self.shape = grid.shape
         self.dimension = dimension
+        self.dimensions = len(grid.shape)
 
-        self.max_work_group_size = max_work_group_size
-        self.max_compute_units = 40  # Todo: get this from outside
-        self.max_local_group_sizes = [512, 512, 512]
+        self.device = device
 
-        self.grid_size = reduce(mul, self.shape)
-        self.local_size = self.compute_local_group_size()
         self.global_size, self.global_offset = self.compute_global_size()
-        self.local_size = self.compute_local_group_size()
-        self.padding = [0 for _ in range(len(shape))]
+        self.local_size = OclTools.compute_local_size_1d()
 
         self.kernel_name = "boundary_copy_kernel_{}d_dim_{}".format(len(halo), dimension)
 
     def compute_global_size(self):
-        global_size = [0 for _ in self.grid.shape]
-        global_offset = [0 for _ in self.grid.shape]
-
-        for other_dim in range(len(self.grid.shape)):
-            if other_dim < self.dimension:
-                global_size[other_dim] = self.grid.shape[other_dim] - 2 * self.halo[other_dim]
-                global_offset[other_dim] = self.halo[other_dim]
-            elif other_dim == self.dimension:
-                global_size[other_dim] = self.halo[other_dim]
-                global_offset[other_dim] = 0
-            else:
-                global_size[other_dim] = self.grid.shape[other_dim]
-                global_offset[other_dim] = 0
-
-        return global_size, global_offset
-
-    def compute_local_size_1d(self):
-        return (min(self.grid_size, self.max_local_group_sizes[0], self.shape[0]/2), )
-
-    def compute_local_size_2d(self):
-        d0_size, d1_size = 1, 1
-
-        if self.grid_size < self.max_work_group_size:
-            return self.shape
-
-        while True:
-            if self.shape[0] % 2 == 1:
-                d0_size = 1
-            else:
-                d0_size = min(self.max_local_group_sizes[0], d0_size * 2)
-            if self.grid_size <= d0_size * d1_size:
-                return d0_size, d1_size
-
-            if self.shape[1] % 2 == 1:
-                d1_size = 1
-            else:
-                d1_size = min(self.max_local_group_sizes[1], d1_size * 2)
-
-            if self.grid_size - d0_size * d1_size <= 0:
-                return d0_size, d1_size
-
-            if d0_size == self.shape[0] or d1_size == self.shape[1]:
-                return d0_size, d1_size
-
-    def compute_local_size_3d(self):
-        d0_size, d1_size, d2_size = 1, 1, 1
-
-        if self.grid_size < self.max_work_group_size:
-            return self.shape
-
-        while True:
-            if self.shape[0] % 2 == 1:
-                d0_size = 1
-            else:
-                d0_size = min(self.max_local_group_sizes[0], d0_size * 2)
-            if self.grid_size <= d0_size * d1_size * d2_size:
-                return d0_size, d1_size, d2_size
-
-            if self.shape[1] % 2 == 1:
-                d1_size = 1
-            else:
-                d1_size = min(self.max_local_group_sizes[1], d1_size * 2)
-            if self.grid_size <= d0_size * d1_size * d2_size:
-                return d0_size, d1_size, d2_size
-
-            if self.shape[2] % 2 == 1:
-                d2_size = 1
-            else:
-                d2_size = min(self.max_local_group_sizes[2], d2_size * 2)
-            if self.grid_size <= d0_size * d1_size * d2_size:
-                return d0_size, d1_size, d2_size
-
-            if d0_size == self.shape[0] or d1_size == self.shape[1] or d2_size == self.shape[2]:
-                return d0_size, d1_size, d2_size
-
-    def compute_local_group_size(self):
-        if len(self.shape) == 1:
-            return self.compute_local_size_1d()
-        elif len(self.shape) == 2:
-            return self.compute_local_size_2d()
-        elif len(self.shape) == 3:
-            return self.compute_local_size_3d()
-        else:
-            raise StencilException("ocl stencils must be 1, 2 or 3 dimensions")
-
-    def make_kernel_template(self):
-        """
-        a boundary kernel will be created, one for each dimension. That kernel
-        will iterate over all other indices of the other dimensions, bounded by those points
-        that have been handled by a previous kernel.
-        The global_size will the entirety of the end planes in the minor side of the dimension
-        :return:
-        """
-
-        boundary_kernel = FunctionDecl()
-        kernel_call = KernelCall()
-        template_string = """
-            __kernel void {kernel_name}(__global const float* in_grid, __global float* out_grid, __local float* block) {
-                #define global_array_macro(d0, d1) ((d1)+((d0) * {actual_width}))
-                int minor_global_index = get_global_id(1) + get_global_id(0) * {actual_width};
-                int major_global_index = (get_global_id(1) + {interior_width} + get_global_id(0) * {actual_width};
-
-                out_grid[minor_global_index] = in_grid[minor_global_index];
-                memfence(CLK_GLOBAL_MEM_FENCE);
-                out_grid[major_global_index] = in_grid[major_global_index];
-            };
-
-            )))
-        """
+        dimension_sizes = [x for x in self.shape]
+        dimension_offsets = [0 for _ in self.shape]
+        for other_dimension in range(self.dimension):
+            dimension_sizes[other_dimension] -= (2 * self.halo[other_dimension])
+            dimension_offsets.append(self.halo[other_dimension])
+        dimension_sizes[other_dimension] = self.halo[other_dimension]
+        return dimension_sizes, dimension_offsets
 
 if __name__ == '__main__':
     import itertools
