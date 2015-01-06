0e852036a1889b388374f25014b916785a3dd6aa
chick <chick@qrhino.com>
2015-01-06 10:30:05 -0800

    Fixed Travis fail because test did not specify specific gpu device and must have been getting a different kind on travis, changed to use a mock

--- test/test_boundary_kernel.py
+++ test/test_boundary_kernel.py
@@ -7,6 +7,7 @@ from ctree.c.nodes import If, Constant
 
 from stencil_code.backend.ocl_boundary_copier import OclBoundaryCopier
 from stencil_code.stencil_exception import StencilException
+from test.fixtures import MockIrisPro
 
 
 class TestBoundaryKernel(unittest.TestCase):
@@ -24,30 +25,30 @@ class TestBoundaryKernel(unittest.TestCase):
         halo = [3, 3]
         grid = numpy.ones(shape).astype(numpy.float32)
 
-        bk = OclBoundaryCopier(halo, grid, 0)
+        bk = OclBoundaryCopier(halo, grid, 0, device=MockIrisPro)
         self.assertEqual(bk.global_size, [3, 7])
         self.assertEqual(bk.global_offset, [0, 0])
 
-        bk = OclBoundaryCopier(halo, grid, 1)
+        bk = OclBoundaryCopier(halo, grid, 1, device=MockIrisPro)
         self.assertEqual(bk.global_size, [96, 3])
         self.assertEqual(bk.global_offset, [3, 0])
 
     def test_exception_for_bad_halo_grid_values(self):
         with self.assertRaises(StencilException) as context:
-            OclBoundaryCopier([2, 3], numpy.ones([4, 423]), 1)
+            OclBoundaryCopier([2, 3], numpy.ones([4, 423]), 1, device=MockIrisPro)
         self.assertTrue(self.__exception_contains("can't span", context.exception))
 
         with self.assertRaises(StencilException) as context:
-            OclBoundaryCopier([2, 3, 4], numpy.ones([4, 423]), 1)
+            OclBoundaryCopier([2, 3, 4], numpy.ones([4, 423]), 1, device=MockIrisPro)
         self.assertTrue(self.__exception_contains("can't apply", context.exception))
 
         with self.assertRaises(StencilException) as context:
-            OclBoundaryCopier([0, 3], numpy.ones([4, 423]), 1)
+            OclBoundaryCopier([0, 3], numpy.ones([4, 423]), 1, device=MockIrisPro)
         self.assertTrue(self.__exception_contains("can't be bigger", context.exception))
 
     def test_virtual_global_size(self):
         grid = numpy.ones([17, 513])
-        bk = OclBoundaryCopier([2, 2], grid, 0)
+        bk = OclBoundaryCopier([2, 2], grid, 0, device=MockIrisPro)
         print("global_size {} local_size {} virtual_global_size {}".format(
             bk.global_size, bk.local_size, bk.virtual_global_size
         ))
@@ -55,35 +56,35 @@ class TestBoundaryKernel(unittest.TestCase):
         self.assertEqual(bk.virtual_global_size, [2, 514])
 
     def test_gen_index_in_bounds_conditional(self):
-        bk = OclBoundaryCopier([2, 2], numpy.ones([512, 513]), 1)
+        bk = OclBoundaryCopier([2, 2], numpy.ones([512, 513]), 1, device=MockIrisPro)
         self.assertTrue(
             type(bk.gen_index_in_bounds_conditional(Constant(1))) == Constant
         )
 
-        bk = OclBoundaryCopier([2, 2, 2], numpy.ones([512, 512, 513]), 1)
+        bk = OclBoundaryCopier([2, 2, 2], numpy.ones([512, 512, 513]), 1, device=MockIrisPro)
         bk.global_size = [3, 3, 3]
         bk.local_size = [4, 4, 4]
         self.assertTrue(
             type(bk.gen_index_in_bounds_conditional(Constant(1))) == If
         )
         with self.assertRaises(StencilException) as context:
-            _ = OclBoundaryCopier([2, 2], numpy.ones([512, 512]), dimension=7)
+            _ = OclBoundaryCopier([2, 2], numpy.ones([512, 512]), dimension=7, device=MockIrisPro)
         self.assertTrue("too big" in context.exception.args[0])
 
-        bk = OclBoundaryCopier([2, 2], numpy.ones([512, 512]), 0)
+        bk = OclBoundaryCopier([2, 2], numpy.ones([512, 512]), 0, device=MockIrisPro)
         self.assertTrue(
             type(bk.gen_index_in_bounds_conditional(Constant(1))) == Constant
         )
-        bk = OclBoundaryCopier([2, 2], numpy.ones([512, 513]), 0)
+        bk = OclBoundaryCopier([2, 2], numpy.ones([512, 513]), 0, device=MockIrisPro)
         self.assertTrue(
             type(bk.gen_index_in_bounds_conditional(Constant(1))) == If
         )
-        bk = OclBoundaryCopier([2, 2], numpy.ones([512, 513]), 1)
+        bk = OclBoundaryCopier([2, 2], numpy.ones([512, 513]), 1, device=MockIrisPro)
         self.assertTrue(
             type(bk.gen_index_in_bounds_conditional(Constant(1))) == Constant
         )
 
-        bk = OclBoundaryCopier([2, 2], numpy.ones([512, 513]), 1)
+        bk = OclBoundaryCopier([2, 2], numpy.ones([512, 513]), 1, device=MockIrisPro)
         bk.global_size = [3, 3]
         bk.local_size = [4, 4]
         self.assertTrue(
--- test/test_local_size_computer.py
+++ test/test_local_size_computer.py
@@ -4,18 +4,7 @@ __author__ = 'chick'
 import unittest
 
 from stencil_code.backend.local_size_computer import product, LocalSizeComputer
-
-
-class MockDevice(object):
-    def __init__(self, max_work_group_size=512, max_work_item_sizes=None,
-                 max_compute_units=40):
-        self.max_work_group_size = max_work_group_size
-        self.max_work_item_sizes = max_work_item_sizes if max_work_item_sizes is not None else [512, 512, 512]
-        self.max_compute_units = max_compute_units
-
-MockCPU = MockDevice(1024, [1024, 1, 1], 8)
-MockIrisPro = MockDevice(512, [512, 512, 512], 40)
-
+from test.fixtures import MockCPU, MockIrisPro
 
 class TestLocalSizeComputer(unittest.TestCase):
     def _are_lists_equal(self, list1, list2):
