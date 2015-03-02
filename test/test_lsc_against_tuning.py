ce1f8193bae3a942f85f9a8e499b946565d2d547
Dorthy Luu <dluu@berkeley.edu>
2015-03-02 00:08:30 -0800

    fixing test to compare values properly

--- test/test_lsc_against_tuning.py
+++ test/test_lsc_against_tuning.py
@@ -15,6 +15,7 @@ class LocalSizeTuner(MeasurementInterface):
     max_work_group_size = 1
     shape = [1, 1]
     sizes_to_try = [(1, 1)]
+    final_config = None
 
     def manipulator(self):
         """
@@ -39,25 +40,28 @@ class LocalSizeTuner(MeasurementInterface):
         else:
             ratio = surface_area / float(volume) #want to minimize this ratio
         return Result(time=ratio) #this is a hack, need to write actual MinimizeRatio Objective
+    def save_final_config(self, configuration):
+        """called at the end of tuning"""
+        LocalSizeTuner.final_config = configuration.data['size']
+
 
 if __name__ == '__main__':
-    if sys.argv[1:]:
-        shape = [int(arg) for arg in sys.argv[1:]]
-    else:
-        shape = [64, 64]
+    # if sys.argv[1:]:
+    #     shape = [int(arg) for arg in sys.argv[1:]]
+    # else:
+    shape = [64, 64, 64]
     lsc = LocalSizeComputer(shape)
-    print "shape: ", shape
-    print "max local group sizes: ", lsc.max_local_group_sizes
-    print "max work group size: ",lsc.max_work_group_size
-    print "compute units: ", lsc.compute_units
+    # print "shape: ", shape
+    # print "max local group sizes: ", lsc.max_local_group_sizes
+    # print "max work group size: ",lsc.max_work_group_size
+    # print "compute units: ", lsc.compute_units
     bulky_results = lsc.compute_local_size_bulky()
-    print "lsc compute bulky :", bulky_results[0]
     LocalSizeTuner.max_work_group_size = lsc.max_work_group_size
     LocalSizeTuner.shape = shape
     LocalSizeTuner.sizes_to_try = bulky_results[1]
 
     argparser = opentuner.default_argparser()
-    x = LocalSizeTuner.main(argparser.parse_args())
-    print x
-
-    print "done"
+    LocalSizeTuner.main(argparser.parse_args())
+    print LocalSizeTuner.final_config
+    print "lsc compute bulky :", bulky_results[0]
+    assert LocalSizeTuner.final_config == bulky_results[0]
